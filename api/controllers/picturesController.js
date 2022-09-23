e059ffca0088ffa3ce465bb676a6dd2eca640cfe
Federico de Fortuny <fdefortuny@gmail.com>
2022-09-22 23:42:36 -0300

    se crean los metodos detail y create de picturesController

--- api/controllers/picturesController.js
+++ api/controllers/picturesController.js
@@ -1,178 +1,166 @@
 const fs = require("fs");
+const { resolve } = require("path");
 const path = require("path");
-const db = require('../../database/models');
+const db = require("../../database/models");
 
 const picturesController = {
-  detail: (req, res) => {
-    //picture segun id
-    const id = req.params.id;
-    try {
-      const db = JSON.parse(
-        fs.readFileSync(
-          path.resolve(__dirname, "../data/pictures.json"),
-          "utf8"
-        )
-      );
-      const dataToShow = db.find((elm) => elm.id === Number(id));
+	detail: (req, res) => {
+		//picture segun id
+		//sprint 2
+		const product_id = req.params.product_id;
 
-      if (!dataToShow) {
-        return res.status(404).json({
-          msg: "Not found",
-        });
-      }
-      res.send(dataToShow);
-    } catch (error) {
-      res.status(500).json({
-        msg: "Server Error",
-      });
-    }
-  },
-  create: (req, res) => {
-    //agregar una nueva imagen a la bd
-    //sprint 2
-    try{
-      const picture = await db.picture.create({
-          title: req.body.title,
-          description: req.body.description,
-          genre: req.body.genre,
-          rating: req.body.rating,
-          awards: req.body.awards,
-          length: req.body.length,
-          genre_id: req.body.genre_id,
-          release_date: req.body.release_date,
-      });
-      res.send(movies);
-  }catch(error) {
+		db.product
+			.findAll(
+				{
+					attributes: ["img", "description"],
+				},
+				{
+					where: {
+						product_id,
+					},
+				}
+			)
+			.then((result) => {
+				console.log(result);
+				return res.send(result);
+			})
+			.catch((error) => {
+				console.log(error);
+				return res.send("no existe ninguna imagen para ese producto");
+			});
+	},
+
+	create: (req, res) => {
+		//agregar una nueva imagen a la bd
+		//sprint 2
+		const { img, description } = req.body;
+
+		if (!img) {
+			res.send("La url de la imagen es obligatorio");
+		}
+
+    let product_id;
+		try {
+			product_id = Number(req.params.product_id);
+		} catch (error) {
       console.log(error);
-      res.send(error);
-  }
+			return res.send("el id debe ser un número");
+		}
 
-  //////sprint 1
+		db.picture
+			.create({
+				id: "default",
+				img,
+				description,
+        product_id
+			})
+			.then(() => {
+				res.send("Imagen creada con éxito.");
+			})
+			.catch((err) => {
+				res.send("error al crear imagen");
+				console.log(err);
+			});
+	},
 
-    let { id, url = "", description = "" } = req.body;
+	modify: (req, res) => {
+    // modificar pictures sprint 2
+      const id = req.params.id;
 
-    if (!id || !url) {
-      return res.status(400)({
-        msg: "Bad Request",
-      });
-    }
-    let newPicture = {
-      id,
-      url,
-      description,
-    };
-    try {
-      const db = JSON.parse(
-        fs.readFileSync(
-          path.resolve(__dirname, "../data/pictures.json"),
-          "utf8"
-        )
-      );
-      db.push(newPicture);
-      fs.writeFileSync(
-        path.resolve(__dirname, "../data/pictures.json"),
-        JSON.stringify(db)
-      );
-      return res.status(200).json(newPicture);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        msg: "Server Error",
-      });
-    }
-  },
-  modify: (req, res) => {
-    const id = req.params.id;
-    try {
-      const db = JSON.parse(
-        fs.readFileSync(
-          path.resolve(__dirname, "../data/pictures.json"),
-          "utf8"
-        )
-      );
-      let picture = db.filter((el) => el.id == id);
-      if (picture) {
-        let data = req.body;
-        data.id = req.params.id;
-        if (data.url) picture[0].url = data.url;
-        if (data.description) picture[0].description = data.description;
 
-        fs.writeFileSync(
-          path.resolve(__dirname, "../data/pictures.json"),
-          JSON.stringify(db)
-        );
-        res.status(200).json(data);
-      } else {
-        return res.status(404).json({
-          status: "error",
-          msg: "Imagen no encontrada",
-        });
-      }
-    } catch (error) {
-      res.status(500).json({
-        status: "Error",
-        msg: "Error en el servidor",
-      });
-    }
-  },
-  deleted: (req, res) => {
-    const { id } = req.params;
-    try {
-      const db = JSON.parse(
-        fs.readFileSync(
-          path.resolve(__dirname, "../data/pictures.json"),
-          "utf8"
-        )
-      );
-      const pictureDeleted = db.find(el => el.id === Number(id)); 
-      const newData = db.filter((el) => el.id !== Number(id));
-      fs.writeFileSync(
-        path.resolve(__dirname, "../data/pictures.json"),
-        JSON.stringify(newData)
-      );
-      return pictureDeleted ? res.status(200).json(pictureDeleted) : res.status(404).json({msg: 'Imagen no encontrada'});
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        msg: "Error",
-      });
-    }
-  },
-  picturesProduct: (req, res) => {
-    try {
-      let data = JSON.parse(
-        fs.readFileSync(
-          path.resolve(__dirname, "../data/products.json"),
-          "utf8"
-        )
-      );
-      //products/id/pictures
-      let dataToShow;
-      if (req.params.id) {
-        const { id } = req.params;
-        dataToShow = data.find((elm) => elm.id === Number(id));
-      }
+    //modificar pictures sprint 1
+		//const id = req.params.id;
+		try {
+			const db = JSON.parse(
+				fs.readFileSync(
+					path.resolve(__dirname, "../data/pictures.json"),
+					"utf8"
+				)
+			);
+			let picture = db.filter((el) => el.id == id);
+			if (picture) {
+				let data = req.body;
+				data.id = req.params.id;
+				if (data.url) picture[0].url = data.url;
+				if (data.description) picture[0].description = data.description;
 
-      if (req.query.product) {
-        //busqueda por ?product=id
-        const { product: queryID } = req.query;
-        dataToShow = data.find((elm) => elm.id === Number(queryID));
-      }
+				fs.writeFileSync(
+					path.resolve(__dirname, "../data/pictures.json"),
+					JSON.stringify(db)
+				);
+				res.status(200).json(data);
+			} else {
+				return res.status(404).json({
+					status: "error",
+					msg: "Imagen no encontrada",
+				});
+			}
+		} catch (error) {
+			res.status(500).json({
+				status: "Error",
+				msg: "Error en el servidor",
+			});
+		}
+	},
+	deleted: (req, res) => {
+		const { id } = req.params;
+		try {
+			const db = JSON.parse(
+				fs.readFileSync(
+					path.resolve(__dirname, "../data/pictures.json"),
+					"utf8"
+				)
+			);
+			const pictureDeleted = db.find((el) => el.id === Number(id));
+			const newData = db.filter((el) => el.id !== Number(id));
+			fs.writeFileSync(
+				path.resolve(__dirname, "../data/pictures.json"),
+				JSON.stringify(newData)
+			);
+			return pictureDeleted
+				? res.status(200).json(pictureDeleted)
+				: res.status(404).json({ msg: "Imagen no encontrada" });
+		} catch (error) {
+			console.log(error);
+			res.status(500).json({
+				msg: "Error",
+			});
+		}
+	},
+	picturesProduct: (req, res) => {
+		try {
+			let data = JSON.parse(
+				fs.readFileSync(
+					path.resolve(__dirname, "../data/products.json"),
+					"utf8"
+				)
+			);
+			//products/id/pictures
+			let dataToShow;
+			if (req.params.id) {
+				const { id } = req.params;
+				dataToShow = data.find((elm) => elm.id === Number(id));
+			}
 
-      if (dataToShow) {
-        return res.status(200).json(dataToShow["gallery"]);
-      } else {
-        return res.status(404).json({
-          msg: 'Producto no encontrado'
-        });
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        msg: "Server Error",
-      });
-    }
+			if (req.query.product) {
+				//busqueda por ?product=id
+				const { product: queryID } = req.query;
+				dataToShow = data.find((elm) => elm.id === Number(queryID));
+			}
 
-  },
+			if (dataToShow) {
+				return res.status(200).json(dataToShow["gallery"]);
+			} else {
+				return res.status(404).json({
+					msg: "Producto no encontrado",
+				});
+			}
+		} catch (error) {
+			console.log(error);
+			res.status(500).json({
+				msg: "Server Error",
+			});
+		}
+	},
 };
 module.exports = picturesController;
