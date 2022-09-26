ac3713afa28e38393160f3683dbeddcc84ec093a
Federico de Fortuny <fdefortuny@gmail.com>
2022-09-26 17:00:28 -0300

    refactor picturesRouter

--- api/routes/picturesRoutes.js
+++ api/routes/picturesRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const router = express.Router();
-const {detail,create,modify,deleted,picturesProduct} = require(path.resolve(__dirname,'../controllers/picturesController'))
+const {detail,create,modify,deleted,picturesProduct} = require(path.resolve(__dirname,'../controllers/picturesController'));
 
-router.get('/',picturesProduct) 
-router.get('/:id',detail)
-router.post('/',create) 
-router.put('/:id',modify)
-router.delete('/:id',deleted)
+router.get('/',picturesProduct);
+router.get('/:id',detail);
+router.post('/',create);
+router.put('/:id',modify);
+router.delete('/:id',deleted);
 
 module.exports = router;
