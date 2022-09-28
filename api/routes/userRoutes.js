4b381ecb887ec382ead055a8f1ed712da8d88396
Román Ferreira <romanferreira24@gmail.com>
2022-09-27 23:26:52 -0300

    Test1 solucion error git

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -7,11 +7,3 @@ const verifyJWT = require('../middlewares/verifyJWT');
 
 
 
-router.post("/login",userController.login);
-router.get("/"/**,verifyJWT,isAdmin */,userController.list);
-router.get("/:id"/**,verifyJWT/**,isAdmin */,userController.searchById);
-router.post("/"/**,verifyJWT/**,isGod */,userController.createUser);
-router.put("/:id"/**,verifyJWT/**,isGod */,userController.modifyUser);
-router.delete("/:id"/**,verifyJWT/**,isGod */,userController.delete);
-
-module.exports = router;
